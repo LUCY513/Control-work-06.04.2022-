4cadf420f15ca92c1656e7d9f42f31bf19e482b3
LUCY <Lucy@example.com>
2022-04-05 23:49:28 +0300

    Добавили рабочие переменные

---
+++ ControlWork/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+// 1. 
+Random countElementsArray = new Random();
+// 2.
+int N = countElementsArray.Next(0, 4);
+// 3. 
+Console.WriteLine("<<Ввыводит три случайных символа из списка имеющихся элементов>>");
+Console.ReadLine();
+// 4.                         i =     0        1      2      3     4     5         6         7      8
+string[] arrayChar = new string[] { "♥GB♥ ", ":з ", "o_o ", "☼ ", "Z ", ":) ", ":-:-:-: ", "♦♦♦ ", "TS " };
+// 5.
+string[] arrayGet = new string[N];
